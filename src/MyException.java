40db08454090f78872dc46f57d6f1e332fe2f38e
Şeyma Yılmaz <seyma.yilmaz3@ogr.sakarya.edu.tr>
2017-06-01 22:40:17 +0200

    Initial commit

---
+++ src/MyException.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Şeyma Yılmaz on 1.6.2017.
+ */
+public class MyException extends Exception {
+
+    @Override
+    public String getMessage() {
+        return "My Exception :)";
+    }
+}
